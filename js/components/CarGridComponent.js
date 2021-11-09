58ffa8687cb24db6cf4d3121a07bd4f0c2067a2e
Miroslav I <styylzeh@gmail.com>
2021-11-09 16:13:46 +0200

    [6]: getting cars from server through API to component

--- js/components/CarGridComponent.js
+++ js/components/CarGridComponent.js
@@ -7,8 +7,29 @@ class CarGridComponent {
     this.init();
   }
 
+  getCars = () => API.fetchCars(this.saveCars, this.showError);
+
+  saveCars = (cars) => {
+    this.state.cars = cars;
+
+    this.render();
+  };
+
+  showError = (error) => {
+    alert(error);
+  };
+
   init = () => {
-    this.htmlElement = document.createElement("article");
-    this.htmlElement.className = "row g-3";
+    this.getCars();
+
+    this.render();
+  };
+
+  render = () => {
+    if (this.state.cars.length === 0) {
+      this.htmlElement.innerHTML = `siunčiama...`;
+    } else {
+      this.htmlElement.innerHTML = `parsiusta`;
+    }
   };
 }
