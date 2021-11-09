80c7dd44c3b08995c01158be8246cdca5dbfe4db
Miroslav I <styylzeh@gmail.com>
2021-11-09 21:41:06 +0200

    [11]some edits

--- js/components/CarCardComponent.js
+++ js/components/CarCardComponent.js
@@ -1,10 +1,10 @@
 class CarCardComponent {
+  static USD_EUR = 1.16;
+
   constructor(props) {
     this.props = props;
     this.init();
   }
-
-  static USD_EUR = 1.16;
 
   currConverted = (curr, amount) => {
     let convertedPrice;
@@ -12,10 +12,10 @@ class CarCardComponent {
     switch (curr) {
       case "$":
         convertedPrice = amount * CarCardComponent.USD_EUR;
-        pricePlaceHolder = `${convertedPrice} € `;
+        pricePlaceHolder = `${convertedPrice}`;
         break;
       case "€":
-        return (pricePlaceHolder = `${amount} €`);
+        return (pricePlaceHolder = `${amount}`);
     }
     return `<div>
         <span>${pricePlaceHolder}</span>
@@ -29,18 +29,18 @@ class CarCardComponent {
     this.htmlElement = document.createElement("div");
     this.htmlElement.className = "card shadow position-relative";
     this.htmlElement.innerHTML = `
-    <div class="card">
+    <div class="card d-flex flex-column">
     <div class="card-body">
       <h5 class="card-title">${brand}</h5>
       <ul>
         <li>${model}</li>
         <li>${year}</li>
         <li>${fuelTypes}</li>
-        <li>Price:${this.currConverted(price.currency, price.amount)}</li>
+        <li>Price:${this.currConverted(price.currency, price.amount)} €</li>
      </ul>
       <img src="${imgSrc}" />
     </div>
-    <button class="btn btn-sm btn-danger w-25 del-btn"> X </button>
+    <button class="btn btn-sm btn-danger w-25 del-btn position-absolute justify-self-center align-self-center "> X </button>
   </div>`;
 
     this.htmlElement
